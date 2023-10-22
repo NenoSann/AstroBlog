1539b8f68ad31112526373b1688b9a9ed362c8a1
LiangHeng <lianghengcn@gmail.com>
2023-10-23 00:20:33 +0800

    feat: change the layout from grid to flex(due to responsive problem)

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex w-1/3 bg-[rgba(255,255,255,0.4)] rounded-md overflow-hidden max-h-120">
+        <div>
+            <img :src="image_url" alt="" class=" flex-grow-0">
+        </div>
+        <div class=" flex-grow">
+            something
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    image_url: String,
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/layouts/BlogList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex flex-col rounded-lg col-span-1 row-span-6 gap-6 basis-4/5 overflow-auto">
+        <BlogCard v-for="Blog in BlogList" :image_url="Blog.image_url"></BlogCard>
+    </div>
+</template>
+
+<script setup>
+import BlogCard from '../components/BlogCard.vue';
+const BlogList = [{
+    image_url: 'src/assets/blog1.png'
+}, { image_url: 'src/assets/blog2.png' }]
+
+</script>
