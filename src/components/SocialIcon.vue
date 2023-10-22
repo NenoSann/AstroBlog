ea771b00ef57166eb174d4e5e6e87098e07a5ffd
LiangHeng <lianghengcn@gmail.com>
2023-10-22 19:55:00 +0800

    feat: add personal info content

---
+++ src/components/SocialIcon.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class=" flex gap-6 justify-center items-center">
+        <div v-for="icon in IconArray" class=" w-8">
+            <a :href="icon.ref">
+                <img :src="icon.src">
+            </a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    IconArray: Array,
+})
+</script>
