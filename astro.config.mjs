3c0e85bb060806c77e56ed0206d74b6494215063
LiangHeng <lianghengcn@gmail.com>
2023-10-28 19:01:52 +0800

    try to add github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://NenoSann.github.io',
+  base: '/AstroBlog',
   integrations: [tailwind(), vue()]
 });
