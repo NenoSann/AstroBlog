36f6d82f19667a98808cb6dd258d880c47f4dc46
LiangHeng <lianghengcn@gmail.com>
2023-10-28 19:35:46 +0800

    Revert "try to add github.io"
    This reverts commit 3c0e85bb060806c77e56ed0206d74b6494215063.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://NenoSann.github.io',
-  base: '/AstroBlog',
   integrations: [tailwind(), vue()]
 });
