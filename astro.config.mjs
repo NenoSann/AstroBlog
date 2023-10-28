e05202320086d258c99748c8d5f9c77eee00fd71
LiangHeng <lianghengcn@gmail.com>
2023-10-28 19:11:35 +0800

    try to fix url broken

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://NenoSann.github.io',
-  base: '/AstroBlog',
   integrations: [tailwind(), vue()]
 });
